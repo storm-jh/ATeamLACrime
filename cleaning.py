40c4669f9fec3092e838ee2b0f2e2cb82dafadd3
stormjh <stormharland@gmail.com>
2025-12-09 19:43:42 +0000

    added dropping of long/lat=0

--- cleaning.py
+++ cleaning.py
@@ -3,9 +3,9 @@ import pandas as pd
 df = pd.read_csv(r'ATeamLACrime\Crime_Data_from_2020_to_Present.csv')
 
 ## Just checking stuff
-#print(df.info)
-#print(df.dtypes)
-#print(df.columns)
+print(df.info)
+print(df.dtypes)
+print(df.columns)
 
 
 ### Setting date and time columns to datetime
@@ -15,14 +15,21 @@ df1=df.astype({"Date Rptd":"datetime64[ns]",
             "TIME OCC":"datetime64[ns]"})
 
 ### setting negative values in Vict Age column to 0
+
 df1["Vict Age"] = df1["Vict Age"].clip(lower=0)
 
-### Checking it all worked
-#print(df1["Vict Age"].min())
-#print("----------")
-#print(df1.dtypes)
+### Removing 2000 or so results with 0 for both longtidue and lattitude
+df1 = df1[(df1["LAT"] != 0) | (df["LON"] != 0)]
 
-## Saving cleaned CSV so we're all using the same dataset, but 
+### Checking it all worked
+#print(df1.dtypes)
+#print(df1["Vict Age"].min())
+#print(df1["LAT"].min())
+#print("----------")
+
+
+
+## Saving cleaned CSV so we're all using the same dataset, though 
 ## this changes date/time occured coulumns back to objects.
 
 #df1.to_csv('crimedata.csv', index=False)
