96429ba658de21fbb19a0a4bf34af0c5ccbad9ed
stormjh <stormharland@gmail.com>
2025-12-11 20:02:14 +0000

    notes for dashboard and typo in cleaning

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-df = pd.read_csv(r'ATeamLACrime\Crime_Data_from_2020_to_Present.csv')
+df = pd.read_csv(r'Crime_Data_from_2020_to_Present.csv', low_memory=False)
 
 ## Just checking stuff
 #print(df.info)
@@ -19,7 +19,7 @@ df1=df.astype({"Date Rptd":"datetime64[ns]",
 df1["Vict Age"] = df1["Vict Age"].clip(lower=0)
 
 ### Removing 2000 or so results with 0 for both longtidue and lattitude
-df1 = df1[(df1["LAT"] != 0) | (df["LON"] != 0)]
+df1 = df1[(df1["LAT"] != 0) | (df1["LON"] != 0)]
 
 
 
@@ -32,9 +32,15 @@ df1 = df1[(df1["LAT"] != 0) | (df["LON"] != 0)]
 
 
 ## Saving cleaned CSV so we're all using the same dataset, though 
-## this changes date/time occured coulumns back to objects.
+## this changes date/time occured coulumns back to objects, use above code
+## and df1 for analysis, or uncomment below to save your file then use that 
+## one for dashboarding software.
 
-#df1.to_csv('crimedata.csv', index=False)
+#df1.to_csv('crimedatacleaned.csv', index=False)
 
-#df2=pd.read_csv(r'ATeamLACrime/crimedata.csv')
+##checking csv export
+#df2=pd.read_csv(r'crimedatacleaned.csv')
 #print(df2.dtypes)
+#print(df2.head)
+
+
